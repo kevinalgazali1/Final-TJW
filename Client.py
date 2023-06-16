7489f731894b14e6328d206b1cfa08b7397ef517
kevinalgazali1 <113749025+kevinalgazali1@users.noreply.github.com>
2023-06-16 23:10:50 +0800

    yang di komen jangan di hapus

--- Client.py
+++ Client.py
@@ -1,116 +1,198 @@
-import tkinter as tk
-from tkinter import filedialog
-import socket
-import os
-import struct
-import threading
+# import tkinter as tk
+# from tkinter import filedialog
+# from ftplib import FTP
 
-TCP_IP = "192.168.6.230"  # Only a local server
-TCP_PORT = 1456  # Just a random choice
-BUFFER_SIZE = 1024  # Standard choice
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# class FTPClientGUI:
+#     def _init_(self, root):
+#         self.root = root
+#         self.root.title("FTP Client")
+        
+#         # Membuat antarmuka pengguna
+#         self.create_widgets()
+        
+#         # Membuat objek FTP
+#         self.ftp = FTP()
+    
+#     def create_widgets(self):
+#         # Label untuk server
+#         server_label = tk.Label(self.root, text="Server:")
+#         server_label.pack()
 
+#         # Entry untuk input server
+#         self.server_entry = tk.Entry(self.root)
+#         self.server_entry.pack()
 
-def connect_ftp():
-    # Connect to the server
-    print("Sending server request...")
+#         # Label untuk username
+#         username_label = tk.Label(self.root, text="Username:")
+#         username_label.pack()
+
+#         # Entry untuk input username
+#         self.username_entry = tk.Entry(self.root)
+#         self.username_entry.pack()
+
+#         # Label untuk password
+#         password_label = tk.Label(self.root, text="Password:")
+#         password_label.pack()
+
+#         # Entry untuk input password
+#         self.password_entry = tk.Entry(self.root, show="*")
+#         self.password_entry.pack()
+
+#         # Tombol untuk menghubungkan ke server
+#         connect_button = tk.Button(self.root, text="Connect", command=self.connect_to_server)
+#         connect_button.pack()
+
+#         # Tombol untuk mengunggah file
+#         upload_button = tk.Button(self.root, text="Upload", command=self.upload_file)
+#         upload_button.pack()
+
+#         # Tombol untuk mengunduh file
+#         download_button = tk.Button(self.root, text="Download", command=self.download_file)
+#         download_button.pack()
+
+#     def connect_to_server(self):
+#         # Mengambil informasi server, username, dan password dari input pengguna
+#         server = self.server_entry.get()
+#         username = self.username_entry.get()
+#         password = self.password_entry.get()
+
+#         # Menghubungkan ke server FTP
+#         try:
+#             self.ftp.connect(server)
+#             self.ftp.login(username, password)
+#             print("Connected to FTP server")
+#         except Exception as e:
+#             print("Error connecting to FTP server:", str(e))
+
+#     def upload_file(self):
+#         # Memilih file yang akan diunggah menggunakan dialog
+#         file_path = filedialog.askopenfilename()
+
+#         # Memeriksa apakah file dipilih
+#         if file_path:
+#             try:
+#                 # Mengunggah file ke server FTP
+#                 file_name = file_path.split("/")[-1]
+#                 with open(file_path, "rb") as file:
+#                     self.ftp.storbinary("STOR " + file_name, file)
+#                 print("File uploaded successfully")
+#             except Exception as e:
+#                 print("Error uploading file:", str(e))
+
+#     def download_file(self):
+#         # Memilih file yang akan diunduh menggunakan dialog
+#         file_path = filedialog.asksaveasfilename()
+
+#         # Memeriksa apakah file disimpan dengan nama yang valid
+#         if file_path:
+#             try:
+#                 # Mengunduh file dari server FTP
+#                 file_name = file_path.split("/")[-1]
+#                 with open(file_path, "wb") as file:
+#                     self.ftp.retrbinary("RETR " + file_name, file.write)
+#                 print("File downloaded successfully")
+#             except Exception as e:
+#                 print("Error downloading file:", str(e))
+
+# # if _name_ == "_main_":
+#     # Membuat instance dari Tkinter
+#     root = tk.Tk()
+    
+#     # Membuat objek FTPClientGUI
+#     ftp_client_gui = FTPClientGUI(root)
+    
+#     # Menjalankan aplikasi GUI
+#     root.mainloop()
+
+from ftplib import FTP
+
+# Konfigurasi server FTP
+FTP_HOST = "192.168.1.13"  # Alamat server FTP
+FTP_PORT = 21  # Port server FTP
+FTP_USER = "vin"  # Nama pengguna FTP
+FTP_PASS = "1234"  # Kata sandi pengguna FTP
+
+def upload_file(file_name):
+    # Upload file ke server FTP
     try:
-        s.connect((TCP_IP, TCP_PORT))
-        message_label.config(text="Connection successful")
-    except:
-        message_label.config(text="Connection unsuccessful. Make sure the server is online.")
-
-
-import threading
-
-def upload_file():
-    # Upload a file
-    file_path = filedialog.askopenfilename()
-    if file_path:
-        print("\nUploading file: {}...".format(file_path))
-        try:
-            # Check the file exists
-            if not os.path.isfile(file_path):
-                print("File does not exist. Make sure the file name was entered correctly.")
-                message_label.config(text="File does not exist. Make sure the file name was entered correctly.")
-                return
-
-            # Disable upload button during file upload
-            upload_button.config(state="disabled")
-
-            # Create a new thread for file upload
-            upload_thread = threading.Thread(target=upload_file_thread, args=(file_path,))
-            upload_thread.start()
-
-        except Exception as e:
-            print("Error uploading file:", str(e))
-            message_label.config(text="Error uploading file")
-
-def upload_file_thread(file_path):
-    try:
-        # Make upload request
-        s.send(b"UPLD")
-
-        # Wait for server acknowledgement
-        server_response = s.recv(BUFFER_SIZE)
-        if server_response == b"OK":
-            # Send file name size and file name
-            s.send(struct.pack("h", len(file_path)))
-            s.send(file_path.encode())
-
-            # Wait for server response
-            server_response = s.recv(BUFFER_SIZE)
-            if server_response == b"OK":
-                # Send file size
-                file_size = os.path.getsize(file_path)
-                s.send(struct.pack("i", file_size))
-
-                # Send the file in chunks defined by BUFFER_SIZE
-                # Doing it this way allows for unlimited potential file sizes to be sent
-                with open(file_path, "rb") as file:
-                    print("\nSending...")
-                    sent_bytes = 0
-                    while sent_bytes < file_size:
-                        chunk = file.read(BUFFER_SIZE)
-                        s.send(chunk)
-                        sent_bytes += len(chunk)
-
-                # Get upload performance details
-                upload_time = struct.unpack("f", s.recv(4))[0]
-                upload_size = struct.unpack("i", s.recv(4))[0]
-                print("\nSent file: {}\nTime elapsed: {}s\nFile size: {}b".format(file_path, upload_time, upload_size))
-                message_label.config(text="File uploaded successfully")
-            else:
-                print("Error sending file size")
-                message_label.config(text="Error sending file size")
-        else:
-            print("Error sending file details")
-            message_label.config(text="Error sending file details")
-
+        ftp = FTP()
+        ftp.connect(FTP_HOST, FTP_PORT)
+        ftp.login(FTP_USER, FTP_PASS)
+        ftp.storbinary("STOR " + file_name, open(file_name, "rb"))
+        ftp.quit()
+        print("File uploaded successfully.")
     except Exception as e:
         print("Error uploading file:", str(e))
-        message_label.config(text="Error uploading file")
 
-    # Enable upload button after file upload is finished
-    upload_button.config(state="normal")
+def list_files():
+    # Mendapatkan daftar file yang tersedia di server FTP
+    try:
+        ftp = FTP()
+        ftp.connect(FTP_HOST, FTP_PORT)
+        ftp.login(FTP_USER, FTP_PASS)
+        file_list = ftp.nlst()
+        for file_name in file_list:
+            print(file_name)
+        ftp.quit()
+    except Exception as e:
+        print("Error retrieving file list:", str(e))
 
+def download_file(file_name):
+    # Download file dari server FTP
+    try:
+        ftp = FTP()
+        ftp.connect(FTP_HOST, FTP_PORT)
+        ftp.login(FTP_USER, FTP_PASS)
+        ftp.retrbinary("RETR " + file_name, open(file_name, "wb").write)
+        ftp.quit()
+        print("File downloaded successfully.")
+    except Exception as e:
+        print("Error downloading file:", str(e))
 
+def delete_file(file_name):
+    # Menghapus file dari server FTP
+    try:
+        ftp = FTP()
+        ftp.connect(FTP_HOST, FTP_PORT)
+        ftp.login(FTP_USER, FTP_PASS)
+        ftp.delete(file_name)
+        ftp.quit()
+        print("File deleted successfully.")
+    except Exception as e:
+        print("Error deleting file:", str(e))
 
-# Membuat window GUI
-window = tk.Tk()
-window.title("FTP Client")
+# Menjalankan FTP client
+print("\nWelcome to the FTP client.")
+print("Available commands:")
+print("UPLOAD file_path  : Upload file to FTP server")
+print("LIST              : List files on FTP server")
+print("DOWNLOAD file     : Download file from FTP server")
+print("DELETE file       : Delete file from FTP server")
+print("QUIT              : Exit FTP client")
 
-# Membuat tombol "Connect" untuk menghubungkan ke FTP server
-connect_button = tk.Button(window, text="Connect", command=connect_ftp)
-connect_button.pack()
+while True:
+    command = input("\nEnter a command: ").split()
+    if not command:
+        continue
 
-# Membuat tombol "Upload File" untuk mengunggah file ke server
-upload_button = tk.Button(window, text="Upload File", command=upload_file)
-upload_button.pack()
-
-# Membuat label untuk menampilkan pesan
-message_label = tk.Label(window, text="")
-message_label.pack()
-
-# Menjalankan GUI
-window.mainloop()
+    if command[0].upper() == "UPLOAD":
+        if len(command) > 1:
+            upload_file(command[1])
+        else:
+            print("Please provide a file path.")
+    elif command[0].upper() == "LIST":
+        list_files()
+    elif command[0].upper() == "DOWNLOAD":
+        if len(command) > 1:
+            download_file(command[1])
+        else:
+            print("Please provide a file name.")
+    elif command[0].upper() == "DELETE":
+        if len(command) > 1:
+            delete_file(command[1])
+        else:
+            print("Please provide a file name.")
+    elif command[0].upper() == "QUIT":
+        break
+    else:
+        print("Command not recognized; please try again.")
--- Server.py
+++ Server.py
@@ -1,71 +1,185 @@
-import socket
+# from pyftpdlib import servers
+# from pyftpdlib.handlers import FTPHandler
+
+# # Konfigurasi server FTP
+# server_address = '192.168.1.13'
+# server_port = 21
+# username = 'vin'
+# password = '1234'
+
+# # Membuat handler FTP
+# handler = FTPHandler
+# handler.authorizer.add_user(username, password, ".", perm='elradfmw')
+
+# # Membuat server FTP
+# server = servers.FTPServer((server_address, server_port), handler)
+
+# # Menjalankan server FTP
+# server.serve_forever()
+
+
+from ftplib import FTP
+import sys
+import time
 import os
 import struct
-import time
 
-TCP_IP = "192.168.6.230"  # Alamat IP server
-TCP_PORT = 1456  # Port server
-BUFFER_SIZE = 1024  # Ukuran buffer
+print("\nWelcome to the FTP server.\n\nTo get started, connect a client.")
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
-print("Server is running and listening...")
+# Konfigurasi server FTP
+FTP_IP = "192.168.1.13"  # Alamat server FTP
+FTP_PORT = 21  # Port server FTP
+BUFFER_SIZE = 1024  # Ukuran buffer standar
+ftp = FTP()
+
+ftp.connect(FTP_IP, FTP_PORT)
+ftp.login()
+print("\nConnected to FTP server: {}:{}".format(FTP_IP, FTP_PORT))
+
+
+def upld():
+    # Kirim pesan setelah server siap menerima detail file
+    conn.send(b"1")
+    # Terima panjang nama file, kemudian nama file
+    file_name_size = struct.unpack("h", conn.recv(2))[0]
+    file_name = conn.recv(file_name_size).decode()
+    # Kirim pesan untuk memberi tahu klien bahwa server siap menerima konten dokumen
+    conn.send(b"1")
+    # Terima ukuran file
+    file_size = struct.unpack("i", conn.recv(4))[0]
+    # Inisialisasi dan masuk ke dalam loop untuk menerima konten file
+    start_time = time.time()
+    output_file = open(file_name, "wb")
+    # Ini melacak berapa byte yang telah kita terima, sehingga kami tahu kapan harus menghentikan loop
+    bytes_received = 0
+    print("\nReceiving...")
+    while bytes_received < file_size:
+        l = conn.recv(BUFFER_SIZE)
+        output_file.write(l)
+        bytes_received += len(l)
+    output_file.close()
+    print("\nReceived file: {}".format(file_name))
+    # Kirim detail kinerja unggah
+    conn.send(struct.pack("f", time.time() - start_time))
+    conn.send(struct.pack("i", file_size))
+    return
+
+
+def list_files():
+    print("Listing files...")
+    # Dapatkan daftar file dalam direktori
+    listing = ftp.nlst()
+    # Kirim jumlah file, sehingga klien tahu apa yang diharapkan (dan menghindari beberapa kesalahan)
+    conn.send(struct.pack("i", len(listing)))
+    total_directory_size = 0
+    # Kirim nama file dan ukurannya sambil menjumlahkan ukuran direktori
+    for i in listing:
+        # Ukuran nama file
+        conn.send(struct.pack("i", sys.getsizeof(i)))
+        # Nama file
+        conn.send(i.encode())
+        # Ukuran konten file
+        file_size = ftp.size(i)
+        conn.send(struct.pack("i", file_size))
+        total_directory_size += file_size
+        # Pastikan klien dan server disinkronkan
+        conn.recv(BUFFER_SIZE)
+    # Jumlah ukuran file dalam direktori
+    conn.send(struct.pack("i", total_directory_size))
+    # Pengecekan akhir
+    conn.recv(BUFFER_SIZE)
+    print("Successfully sent file listing")
+    return
+
+
+def dwld():
+    conn.send(b"1")
+    file_name_length = struct.unpack("h", conn.recv(2))[0]
+    print(file_name_length)
+    file_name = conn.recv(file_name_length).decode()
+    print(file_name)
+    if file_name in ftp.nlst():
+        # Jika file ada, kirim ukuran file
+        file_size = ftp.size(file_name)
+        conn.send(struct.pack("i", file_size))
+    else:
+        # Jika file tidak ada, kirim kode error
+        print("File name not valid")
+        conn.send(struct.pack("i", -1))
+        return
+    # Tunggu persetujuan untuk mengirim file
+    conn.recv(BUFFER_SIZE)
+    # Masuk ke dalam loop untuk mengirim file
+    start_time = time.time()
+    print("Sending file...")
+    content = open(file_name, "rb")
+    # Lagi, pecah menjadi bagian yang ditentukan oleh BUFFER_SIZE
+    l = content.read(BUFFER_SIZE)
+    while l:
+        conn.send(l)
+        l = content.read(BUFFER_SIZE)
+    content.close()
+    # Dapatkan persetujuan dari klien, lalu kirim detail unduhan
+    conn.recv(BUFFER_SIZE)
+    conn.send(struct.pack("f", time.time() - start_time))
+    return
+
+
+def delf():
+    # Kirim persetujuan
+    conn.send(b"1")
+    # Dapatkan detail file
+    file_name_length = struct.unpack("h", conn.recv(2))[0]
+    file_name = conn.recv(file_name_length).decode()
+    # Periksa apakah file ada
+    if file_name in ftp.nlst():
+        conn.send(struct.pack("i", 1))
+    else:
+        # Jika file tidak ada
+        conn.send(struct.pack("i", -1))
+        return
+    # Tunggu konfirmasi penghapusan
+    confirm_delete = conn.recv(BUFFER_SIZE).decode()
+    if confirm_delete == "Y":
+        try:
+            # Hapus file
+            ftp.delete(file_name)
+            conn.send(struct.pack("i", 1))
+        except:
+            # Tidak dapat menghapus file
+            print("Failed to delete {}".format(file_name))
+            conn.send(struct.pack("i", -1))
+    else:
+        # Pengguna membatalkan penghapusan
+        # Server mungkin menerima "N", tetapi else digunakan sebagai pengaman penangkapan kesalahan
+        print("Delete abandoned by client!")
+        return
+
+
+def quit_ftp():
+    # Kirim konfirmasi keluar
+    conn.send(b"1")
+    # Tutup dan restart server
+    conn.close()
+    ftp.quit()
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
 
 while True:
-    conn, addr = s.accept()
-    print("Connection address:", addr)
-
-    # Menerima permintaan dari client
-    request = conn.recv(BUFFER_SIZE)
-    if request == b"UPLD":
-        print("Upload request received.")
-
-        # Mengirimkan konfirmasi ke client
-        conn.send(b"OK")
-
-        # Menerima ukuran nama file
-        file_name_size = struct.unpack("h", conn.recv(2))[0]
-
-        # Menerima nama file
-        file_name = conn.recv(file_name_size).decode()
-        print("Received file name:", file_name)
-
-        # Mengirimkan konfirmasi ke client
-        conn.send(b"OK")
-
-        # Menerima ukuran file
-        file_size = struct.unpack("i", conn.recv(4))[0]
-        print("Received file size:", file_size, "bytes")
-
-        # Menyiapkan path penyimpanan file
-        save_directory = "C:\\Users\\Lenovo\\Downloads\\week14\\week14\\gambar"  # Ganti dengan path yang sesuai
-        save_path = os.path.join(save_directory, file_name)
-
-        # Menerima file dan menyimpannya
-        with open(save_path, "wb") as file:
-            print("Receiving file...")
-            start_time = time.time()
-            remaining_size = file_size
-            while remaining_size > 0:
-                data = conn.recv(min(BUFFER_SIZE, remaining_size))
-                if not data:
-                    break
-                file.write(data)
-                remaining_size -= len(data)
-                end_time = time.time()
-        print("File received and saved successfully.")
-
-        # Menghitung dan mengirimkan detail performa upload ke client
-        upload_time = end_time - start_time # Ganti dengan waktu upload yang sesuai
-        upload_size = file_size
-        conn.send(struct.pack("f", upload_time))
-        conn.send(struct.pack("i", upload_size))
-
-        # Mencetak daftar file dalam direktori penyimpanan
-        file_list = os.listdir(save_directory)
-        print("File list in save directory:")
-        for file in file_list:
-            print(file)
-
-    conn.close()
+    # Masuk ke dalam loop while untuk menerima perintah dari klien
+    print("\n\nWaiting for instruction")
+    data = conn.recv(BUFFER_SIZE).decode()
+    print("\nReceived instruction: {}".format(data))
+    # Periksa perintah dan tanggapi dengan benar
+    if data == "UPLD":
+        upld()
+    elif data == "LIST":
+        list_files()
+    elif data == "DWLD":
+        dwld()
+    elif data == "DELF":
+        delf()
+    elif data == "QUIT":
+        quit_ftp()
+    # Reset data untuk melooping
+    data = None
