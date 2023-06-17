093aa22031ff1140fe02ef4c601ed171cf634d6e
kevinalgazali1 <113749025+kevinalgazali1@users.noreply.github.com>
2023-06-17 18:12:01 +0800

    p

--- Client.py
+++ Client.py
@@ -1,96 +1,3 @@
-# from ftplib import FTP
-
-# # Konfigurasi server FTP
-# FTP_HOST = "192.168.1.13"  # Alamat server FTP
-# FTP_PORT = 21  # Port server FTP
-# FTP_USER = "vin"  # Nama pengguna FTP
-# FTP_PASS = "1234"  # Kata sandi pengguna FTP
-
-# def upload_file(file_name):
-#     # Upload file ke server FTP
-#     try:
-#         ftp = FTP()
-#         ftp.connect(FTP_HOST, FTP_PORT)
-#         ftp.login(FTP_USER, FTP_PASS)
-#         ftp.storbinary("STOR " + file_name, open(file_name, "rb"))
-#         ftp.quit()
-#         print("File uploaded successfully.")
-#     except Exception as e:
-#         print("Error uploading file:", str(e))
-
-# def list_files():
-#     # Mendapatkan daftar file yang tersedia di server FTP
-#     try:
-#         ftp = FTP()
-#         ftp.connect(FTP_HOST, FTP_PORT)
-#         ftp.login(FTP_USER, FTP_PASS)
-#         file_list = ftp.nlst()
-#         for file_name in file_list:
-#             print(file_name)
-#         ftp.quit()
-#     except Exception as e:
-#         print("Error retrieving file list:", str(e))
-
-# def download_file(file_name):
-#     # Download file dari server FTP
-#     try:
-#         ftp = FTP()
-#         ftp.connect(FTP_HOST, FTP_PORT)
-#         ftp.login(FTP_USER, FTP_PASS)
-#         ftp.retrbinary("RETR " + file_name, open(file_name, "wb").write)
-#         ftp.quit()
-#         print("File downloaded successfully.")
-#     except Exception as e:
-#         print("Error downloading file:", str(e))
-
-# def delete_file(file_name):
-#     # Menghapus file dari server FTP
-#     try:
-#         ftp = FTP()
-#         ftp.connect(FTP_HOST, FTP_PORT)
-#         ftp.login(FTP_USER, FTP_PASS)
-#         ftp.delete(file_name)
-#         ftp.quit()
-#         print("File deleted successfully.")
-#     except Exception as e:
-#         print("Error deleting file:", str(e))
-
-# # Menjalankan FTP client
-# print("\nWelcome to the FTP client.")
-# print("Available commands:")
-# print("UPLOAD file_path  : Upload file to FTP server")
-# print("LIST              : List files on FTP server")
-# print("DOWNLOAD file     : Download file from FTP server")
-# print("DELETE file       : Delete file from FTP server")
-# print("QUIT              : Exit FTP client")
-
-# while True:
-#     command = input("\nEnter a command: ").split()
-#     if not command:
-#         continue
-
-#     if command[0].upper() == "UPLOAD":
-#         if len(command) > 1:
-#             upload_file(command[1])
-#         else:
-#             print("Please provide a file path.")
-#     elif command[0].upper() == "LIST":
-#         list_files()
-#     elif command[0].upper() == "DOWNLOAD":
-#         if len(command) > 1:
-#             download_file(command[1])
-#         else:
-#             print("Please provide a file name.")
-#     elif command[0].upper() == "DELETE":
-#         if len(command) > 1:
-#             delete_file(command[1])
-#         else:
-#             print("Please provide a file name.")
-#     elif command[0].upper() == "QUIT":
-#         break
-#     else:
-#         print("Command not recognized; please try again.")
-
 import PySimpleGUI as sg
 from flask import Flask, request, render_template, redirect, send_file, make_response
 from ftplib import FTP
@@ -102,8 +9,8 @@ app = Flask(__name__)
 
 # Konfigurasi FTP
 ftp_host = "192.168.1.14"
-ftp_user = 'alqa'
-ftp_password = 'alqa'
+ftp_user = 'vin'
+ftp_password = '1234'
 
 # Menggunakan lock untuk mengakses variabel bersama
 lock = threading.Lock()
@@ -161,7 +68,7 @@ def run_flask_app():
 
 # GUI Layout
 layout = [
-    [sg.Text("File Bridge - Unduh File", font=("Arial", 14))],
+    [sg.Text("Files - Unduh File", font=("Arial", 14))],
     [sg.Text("Nama File:", size=(10, 1)), sg.InputText(key="file_name"), sg.Button("Unduh", key="download")],
     [sg.Text("Daftar File di FTP:")],
     [sg.Listbox(values=[], size=(40, 10), key="file_list")],
@@ -171,7 +78,7 @@ layout = [
 ]
 
 # Membuat window GUI
-window = sg.Window("File Bridge", layout)
+window = sg.Window("Files", layout)
 
 # Thread untuk menjalankan aplikasi Flask
 flask_thread = threading.Thread(target=run_flask_app)
