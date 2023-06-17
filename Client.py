df9264fe565081b4dfe924917b6339d8604225a0
kevinalgazali1 <113749025+kevinalgazali1@users.noreply.github.com>
2023-06-17 17:31:56 +0800

    client saja yang di pakai

--- Client.py
+++ Client.py
@@ -1,198 +1,221 @@
-# import tkinter as tk
-# from tkinter import filedialog
 # from ftplib import FTP
 
-# class FTPClientGUI:
-#     def _init_(self, root):
-#         self.root = root
-#         self.root.title("FTP Client")
-        
-#         # Membuat antarmuka pengguna
-#         self.create_widgets()
-        
-#         # Membuat objek FTP
-#         self.ftp = FTP()
-    
-#     def create_widgets(self):
-#         # Label untuk server
-#         server_label = tk.Label(self.root, text="Server:")
-#         server_label.pack()
+# # Konfigurasi server FTP
+# FTP_HOST = "192.168.1.13"  # Alamat server FTP
+# FTP_PORT = 21  # Port server FTP
+# FTP_USER = "vin"  # Nama pengguna FTP
+# FTP_PASS = "1234"  # Kata sandi pengguna FTP
 
-#         # Entry untuk input server
-#         self.server_entry = tk.Entry(self.root)
-#         self.server_entry.pack()
+# def upload_file(file_name):
+#     # Upload file ke server FTP
+#     try:
+#         ftp = FTP()
+#         ftp.connect(FTP_HOST, FTP_PORT)
+#         ftp.login(FTP_USER, FTP_PASS)
+#         ftp.storbinary("STOR " + file_name, open(file_name, "rb"))
+#         ftp.quit()
+#         print("File uploaded successfully.")
+#     except Exception as e:
+#         print("Error uploading file:", str(e))
 
-#         # Label untuk username
-#         username_label = tk.Label(self.root, text="Username:")
-#         username_label.pack()
+# def list_files():
+#     # Mendapatkan daftar file yang tersedia di server FTP
+#     try:
+#         ftp = FTP()
+#         ftp.connect(FTP_HOST, FTP_PORT)
+#         ftp.login(FTP_USER, FTP_PASS)
+#         file_list = ftp.nlst()
+#         for file_name in file_list:
+#             print(file_name)
+#         ftp.quit()
+#     except Exception as e:
+#         print("Error retrieving file list:", str(e))
 
-#         # Entry untuk input username
-#         self.username_entry = tk.Entry(self.root)
-#         self.username_entry.pack()
+# def download_file(file_name):
+#     # Download file dari server FTP
+#     try:
+#         ftp = FTP()
+#         ftp.connect(FTP_HOST, FTP_PORT)
+#         ftp.login(FTP_USER, FTP_PASS)
+#         ftp.retrbinary("RETR " + file_name, open(file_name, "wb").write)
+#         ftp.quit()
+#         print("File downloaded successfully.")
+#     except Exception as e:
+#         print("Error downloading file:", str(e))
 
-#         # Label untuk password
-#         password_label = tk.Label(self.root, text="Password:")
-#         password_label.pack()
+# def delete_file(file_name):
+#     # Menghapus file dari server FTP
+#     try:
+#         ftp = FTP()
+#         ftp.connect(FTP_HOST, FTP_PORT)
+#         ftp.login(FTP_USER, FTP_PASS)
+#         ftp.delete(file_name)
+#         ftp.quit()
+#         print("File deleted successfully.")
+#     except Exception as e:
+#         print("Error deleting file:", str(e))
 
-#         # Entry untuk input password
-#         self.password_entry = tk.Entry(self.root, show="*")
-#         self.password_entry.pack()
+# # Menjalankan FTP client
+# print("\nWelcome to the FTP client.")
+# print("Available commands:")
+# print("UPLOAD file_path  : Upload file to FTP server")
+# print("LIST              : List files on FTP server")
+# print("DOWNLOAD file     : Download file from FTP server")
+# print("DELETE file       : Delete file from FTP server")
+# print("QUIT              : Exit FTP client")
 
-#         # Tombol untuk menghubungkan ke server
-#         connect_button = tk.Button(self.root, text="Connect", command=self.connect_to_server)
-#         connect_button.pack()
+# while True:
+#     command = input("\nEnter a command: ").split()
+#     if not command:
+#         continue
 
-#         # Tombol untuk mengunggah file
-#         upload_button = tk.Button(self.root, text="Upload", command=self.upload_file)
-#         upload_button.pack()
+#     if command[0].upper() == "UPLOAD":
+#         if len(command) > 1:
+#             upload_file(command[1])
+#         else:
+#             print("Please provide a file path.")
+#     elif command[0].upper() == "LIST":
+#         list_files()
+#     elif command[0].upper() == "DOWNLOAD":
+#         if len(command) > 1:
+#             download_file(command[1])
+#         else:
+#             print("Please provide a file name.")
+#     elif command[0].upper() == "DELETE":
+#         if len(command) > 1:
+#             delete_file(command[1])
+#         else:
+#             print("Please provide a file name.")
+#     elif command[0].upper() == "QUIT":
+#         break
+#     else:
+#         print("Command not recognized; please try again.")
 
-#         # Tombol untuk mengunduh file
-#         download_button = tk.Button(self.root, text="Download", command=self.download_file)
-#         download_button.pack()
-
-#     def connect_to_server(self):
-#         # Mengambil informasi server, username, dan password dari input pengguna
-#         server = self.server_entry.get()
-#         username = self.username_entry.get()
-#         password = self.password_entry.get()
-
-#         # Menghubungkan ke server FTP
-#         try:
-#             self.ftp.connect(server)
-#             self.ftp.login(username, password)
-#             print("Connected to FTP server")
-#         except Exception as e:
-#             print("Error connecting to FTP server:", str(e))
-
-#     def upload_file(self):
-#         # Memilih file yang akan diunggah menggunakan dialog
-#         file_path = filedialog.askopenfilename()
-
-#         # Memeriksa apakah file dipilih
-#         if file_path:
-#             try:
-#                 # Mengunggah file ke server FTP
-#                 file_name = file_path.split("/")[-1]
-#                 with open(file_path, "rb") as file:
-#                     self.ftp.storbinary("STOR " + file_name, file)
-#                 print("File uploaded successfully")
-#             except Exception as e:
-#                 print("Error uploading file:", str(e))
-
-#     def download_file(self):
-#         # Memilih file yang akan diunduh menggunakan dialog
-#         file_path = filedialog.asksaveasfilename()
-
-#         # Memeriksa apakah file disimpan dengan nama yang valid
-#         if file_path:
-#             try:
-#                 # Mengunduh file dari server FTP
-#                 file_name = file_path.split("/")[-1]
-#                 with open(file_path, "wb") as file:
-#                     self.ftp.retrbinary("RETR " + file_name, file.write)
-#                 print("File downloaded successfully")
-#             except Exception as e:
-#                 print("Error downloading file:", str(e))
-
-# # if _name_ == "_main_":
-#     # Membuat instance dari Tkinter
-#     root = tk.Tk()
-    
-#     # Membuat objek FTPClientGUI
-#     ftp_client_gui = FTPClientGUI(root)
-    
-#     # Menjalankan aplikasi GUI
-#     root.mainloop()
-
+import PySimpleGUI as sg
+from flask import Flask, request, render_template, redirect, send_file, make_response
 from ftplib import FTP
+from io import BytesIO
+import threading
 
-# Konfigurasi server FTP
-FTP_HOST = "192.168.1.13"  # Alamat server FTP
-FTP_PORT = 21  # Port server FTP
-FTP_USER = "vin"  # Nama pengguna FTP
-FTP_PASS = "1234"  # Kata sandi pengguna FTP
+# Inisialisasi aplikasi Flask
+app = Flask(__name__)
 
-def upload_file(file_name):
-    # Upload file ke server FTP
+# Konfigurasi FTP
+ftp_host = "192.168.1.14"
+ftp_user = 'alqa'
+ftp_password = 'alqa'
+
+# Menggunakan lock untuk mengakses variabel bersama
+lock = threading.Lock()
+
+# Fungsi untuk menghubungkan ke FTP
+def get_ftp_connection():
+    ftp = FTP(ftp_host)
+    ftp.login(user=ftp_user, passwd=ftp_password)
+    return ftp
+
+# Fungsi untuk mengunggah file ke server FTP
+def upload_file(file):
     try:
-        ftp = FTP()
-        ftp.connect(FTP_HOST, FTP_PORT)
-        ftp.login(FTP_USER, FTP_PASS)
-        ftp.storbinary("STOR " + file_name, open(file_name, "rb"))
-        ftp.quit()
-        print("File uploaded successfully.")
+        with get_ftp_connection() as ftp:
+            # Mengirim file langsung dari objek FileStorage
+            ftp.storbinary('STOR {}'.format(file.filename), file.stream)
+        
+        with lock:
+            file_list = get_file_list()
+            file_list.append(file.filename)
     except Exception as e:
-        print("Error uploading file:", str(e))
+        # Tangani kesalahan jika terjadi
+        print(f"Error during file upload: {str(e)}")
 
-def list_files():
-    # Mendapatkan daftar file yang tersedia di server FTP
-    try:
-        ftp = FTP()
-        ftp.connect(FTP_HOST, FTP_PORT)
-        ftp.login(FTP_USER, FTP_PASS)
-        file_list = ftp.nlst()
-        for file_name in file_list:
-            print(file_name)
-        ftp.quit()
-    except Exception as e:
-        print("Error retrieving file list:", str(e))
-
+# Fungsi untuk mengunduh file dari server FTP
 def download_file(file_name):
-    # Download file dari server FTP
     try:
-        ftp = FTP()
-        ftp.connect(FTP_HOST, FTP_PORT)
-        ftp.login(FTP_USER, FTP_PASS)
-        ftp.retrbinary("RETR " + file_name, open(file_name, "wb").write)
-        ftp.quit()
-        print("File downloaded successfully.")
-    except Exception as e:
-        print("Error downloading file:", str(e))
+        with get_ftp_connection() as ftp:
+            # Mengunduh file langsung ke objek BytesIO
+            file_data = BytesIO()
+            ftp.retrbinary('RETR {}'.format(file_name), file_data.write)
 
-def delete_file(file_name):
-    # Menghapus file dari server FTP
+            # Mengembalikan data file yang diunduh
+            file_data.seek(0)
+            return file_data.getvalue()  # Mengubah BytesIO menjadi bytes
+    except Exception as e:
+        # Tangani kesalahan jika terjadi
+        print(f"Error during file download: {str(e)}")
+
+# Fungsi untuk mendapatkan daftar file di server FTP
+def get_file_list():
     try:
-        ftp = FTP()
-        ftp.connect(FTP_HOST, FTP_PORT)
-        ftp.login(FTP_USER, FTP_PASS)
-        ftp.delete(file_name)
-        ftp.quit()
-        print("File deleted successfully.")
+        with get_ftp_connection() as ftp:
+            file_list = ftp.nlst()
+
+        return file_list
     except Exception as e:
-        print("Error deleting file:", str(e))
+        # Tangani kesalahan jika terjadi
+        print(f"Error during getting file list: {str(e)}")
+        return []
 
-# Menjalankan FTP client
-print("\nWelcome to the FTP client.")
-print("Available commands:")
-print("UPLOAD file_path  : Upload file to FTP server")
-print("LIST              : List files on FTP server")
-print("DOWNLOAD file     : Download file from FTP server")
-print("DELETE file       : Delete file from FTP server")
-print("QUIT              : Exit FTP client")
+# Fungsi untuk menjalankan aplikasi Flask dalam thread terpisah
+def run_flask_app():
+    app.run(debug=False)
 
+# GUI Layout
+layout = [
+    [sg.Text("File Bridge - Unduh File", font=("Arial", 14))],
+    [sg.Text("Nama File:", size=(10, 1)), sg.InputText(key="file_name"), sg.Button("Unduh", key="download")],
+    [sg.Text("Daftar File di FTP:")],
+    [sg.Listbox(values=[], size=(40, 10), key="file_list")],
+    [sg.Text("", size=(40, 1), key="status_message")],
+    [sg.Button("Unggah File", key="upload")],
+    [sg.Button("Keluar")]
+]
+
+# Membuat window GUI
+window = sg.Window("File Bridge", layout)
+
+# Thread untuk menjalankan aplikasi Flask
+flask_thread = threading.Thread(target=run_flask_app)
+
+# Menjalankan thread Flask
+flask_thread.start()
+
+# Looping utama GUI
 while True:
-    command = input("\nEnter a command: ").split()
-    if not command:
-        continue
+    event, values = window.read()
 
-    if command[0].upper() == "UPLOAD":
-        if len(command) > 1:
-            upload_file(command[1])
-        else:
-            print("Please provide a file path.")
-    elif command[0].upper() == "LIST":
-        list_files()
-    elif command[0].upper() == "DOWNLOAD":
-        if len(command) > 1:
-            download_file(command[1])
-        else:
-            print("Please provide a file name.")
-    elif command[0].upper() == "DELETE":
-        if len(command) > 1:
-            delete_file(command[1])
-        else:
-            print("Please provide a file name.")
-    elif command[0].upper() == "QUIT":
+    if event == sg.WINDOW_CLOSED or event == "Keluar":
         break
-    else:
-        print("Command not recognized; please try again.")
+
+    if event == "download":
+        file_name = values["file_name"]
+        if file_name == "":
+            sg.popup("Mohon masukkan nama file yang ingin diunduh")
+            continue
+
+        file_data = download_file(file_name)
+        if file_data:
+            response = make_response(file_data)
+            response.headers["Content-Disposition"] = f"attachment; filename={file_name}"
+            window["status_message"].update("")
+            sg.popup_scrolled("File berhasil diunduh", title="Status")
+        else:
+            window["status_message"].update("File tidak ditemukan")
+
+    if event == "upload":
+        file_path = sg.popup_get_file("Pilih file yang ingin diunggah", no_window=True)
+        if file_path:
+            with lock:
+                file = open(file_path, "rb")
+                upload_file(file)
+                file.close()
+            window["status_message"].update("File berhasil diunggah")
+        else:
+            window["status_message"].update("Pilih file yang ingin diunggah")
+
+    with lock:
+        file_list = get_file_list()
+        window["file_list"].update(file_list)
+
+# Menutup window GUI dan menghentikan aplikasi Flask
+window.close()
+flask_thread.join()
